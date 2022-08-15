2bb6a58bea2b0b87fd1bab11972bc3ba1a97566a
Manish Ranjan Behera <chaplineleven@gmail.com>
2022-08-15 12:13:44 +0530

    first

---
+++ src/components/CartItem.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const CartItem = ({product, changeQuantity}) => {
+    return (
+        <div className="cart-item">
+            <img src={product.url} alt="cart-item" className="cart-item-image" />
+            <div>
+                <div>
+                    <p className="item-title">{product.title}</p>
+                    <span className="cart-item-price">Rs {product.price}</span>
+                </div>
+                <div>
+                    <p className="item-quantity">Quantity: <span>{product.quantity}</span></p>
+                    <div>
+                     <button className="quantity-btn" onClick={() => changeQuantity(product, '-')}>-</button>
+                     <button className="quantity-btn" onClick={() => changeQuantity(product, '+')}>+</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default CartItem;
